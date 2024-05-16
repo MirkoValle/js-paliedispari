ca22fa1c97e1de9414695be2b84f8b2f0a7ba4ae
MirkoValle <mirko.valle20@gmail.com>
2024-05-16 17:05:31 +0200

    Added function to calculate if the sum its Even or Odd

--- Pari o Dispari/js/script.js
+++ Pari o Dispari/js/script.js
@@ -1,10 +1,21 @@
 const choice = prompt("Scegli pari o dispari")
 const userNumber = Number.parseInt(prompt("Inserisci un numero"), 10);
 const randomNumber = numeroRandom()
-
+const risultato = somma(randomNumber, userNumber)
 
 function numeroRandom() {
     const randomNumber = Math.floor(Math.random() * 5 + 1);
     return randomNumber;
 }
 
+function somma(numeroCasuale, numeroUtente) {
+
+    const sum = numeroCasuale + numeroUtente
+
+    if (sum % 2 == 0) {
+        return "pari";
+    } else {
+        return "dispari";
+    }
+
+}
